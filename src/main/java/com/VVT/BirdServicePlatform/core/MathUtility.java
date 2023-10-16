1dc25ee902ebb2a802a35b424c8cf73c8b66dfcb
bilui2508 <xa30383@gmail.com>
2023-10-16 14:07:19 +0700

    update getf() to see redalert

--- src/main/java/com/VVT/BirdServicePlatform/core/MathUtility.java
+++ src/main/java/com/VVT/BirdServicePlatform/core/MathUtility.java
@@ -24,7 +24,8 @@ public class MathUtility {
             return 1;
         }
 
-        long result = 1;
+        long result = 10;
+        //cố tình làm code màu đỏ
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
